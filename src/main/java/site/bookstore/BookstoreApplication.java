f8dfbc6578ee501fd17b7c7934c71923415512fa
Eeliii <eeli.killstrom@myy.haaga-helia.fi>
2021-02-23 19:16:25 +0200

    Added a new entity to be used in the CRUD repository

--- src/main/java/site/bookstore/BookstoreApplication.java
+++ src/main/java/site/bookstore/BookstoreApplication.java
@@ -2,6 +2,8 @@ package site.bookstore;
 
 
 
+
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +11,8 @@ import org.springframework.context.annotation.Bean;
 
 import site.bookstore.domain.Book;
 import site.bookstore.domain.BookRepository;
+import site.bookstore.domain.Category;
+import site.bookstore.domain.CategoryRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -17,14 +21,16 @@ public class BookstoreApplication {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
-	@Bean public CommandLineRunner demo(BookRepository repository) {
+	@Bean public CommandLineRunner demo(BookRepository repository, CategoryRepository repository2) {
 		return (args) ->{
 			Book book1 = new Book("Self Help for Dummies", "Phony Robbers", 1984, "ISBNLOLXD", 99);
 			Book book2 = new Book("How to Influence Friends and Win People", "Kari Kalmasuo", 2012, "ISBNOMGWTF", 2);
-			
+			Category c1 = new Category("Comedy");
+			Category c2 = new Category("Fiction");
 			repository.save(book1);
 			repository.save(book2);
-			
+			repository2.save(c1);
+			repository2.save(c2);
 		};
 	}
 }
---
+++ src/main/java/site/bookstore/domain/CategoryRepository.java
@@ -0,0 +1,8 @@
+package site.bookstore.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryRepository extends CrudRepository<Category, Long>{
+
+	
+}
